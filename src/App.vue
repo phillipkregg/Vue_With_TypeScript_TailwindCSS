435faa36df51aebbd5bdf6b47bf329915c0087f5
Phil Lackey <phillackey@ip-192-168-1-123.ec2.internal>
2021-05-05 21:37:06 -0500

    Converted project to use TypeScript

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import Vue from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
-  name: 'App',
+export default Vue.extend({
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+});
 </script>
 
 <style>
